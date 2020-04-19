0a40d1ee8845b99ef6e22b7718236d9fbc230ba5
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-20 01:24:09 +0530

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,6 @@
 """
 Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
 """
+a=str(input("Enter your First name:"))
+b=str(input("Enter your Last name:"))
+print (b,a)
