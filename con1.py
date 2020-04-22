27ae296a27afd848f58ab64ee414702f55796594
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-23 00:47:42 +0530

    Create con1.py

---
+++ con1.py
@@ -0,0 +1,12 @@
+'''
+A program to concatenate strings and variables.
+Assign three strings to three different variables.
+Concatenate them and assign the result to a fourth variable.
+Display the concatenated string using the fourth variable.
+'''
+first_name="Amrut"
+middle_name="Mahadev"
+last_name="Badiger"
+print(first_name+middle_name+last_name) # to concatenate variables.
+print("Amrut"+" "+"Mahadev"+" "+"Badiger") # to con. str and add spaces.
+print("Amrut "+"Mahadev "+"Badiger ") # to con.str and add spaces in strs itself.
