a20ab719b9cc28d9ba3071accc617032afd21e72
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-22 23:10:02 +0530

    Create math1.py

---
+++ math1.py
@@ -0,0 +1,12 @@
+'''
+A program to show math expressions.
+'''
+length = 56
+breadth = 34
+height = 24
+area = length*breadth*height
+print(area)
+# a single line code to display other math expressions.
+print( 12+23+45)
+print( 45-23)
+print( 100/10)
