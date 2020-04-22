7f933281d4cf6e2088fb8763dc59f57a55e7de32
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-22 23:29:50 +0530

    Create math2.py

---
+++ math2.py
@@ -0,0 +1,17 @@
+'''
+A program to show the use of unfamiliar math operators
+'''
+age = 40
+age +=5
+left_over =15%6
+a = 78
+a -= 8
+b =40
+b *=5
+c = 100
+c /=5
+print(age)
+print(left_over)
+print(a)
+print(b)
+print(c)
