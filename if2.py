f0692ad2e58a344d6a85eb3ade455cef9602ed9d
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-23 01:26:45 +0530

    Create if2.py

---
+++ if2.py
@@ -0,0 +1,15 @@
+'''
+a program using "if" statements and comparison operators like !=,<,>,<=,>=.
+'''
+
+if 2+3!=4:
+    print("correct")
+if 23<78:
+    print("right")
+if 82>34:
+    print("yes")
+if 45<=67:
+    print("wrong")
+if 32>=32:
+    print("true")
+    
