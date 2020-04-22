e9ed525f5da1296eb20f96ba0ec081645f0d398a
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-22 22:58:33 +0530

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,6 @@
+'''
+a program to prepare a list and display it
+'''
+cities = ["Mumbai", "Bengaluru", "Chandigarh", "New Delhi", "Kolkatta"]
+print(cities)
+print(cities[2]) # code to display the third element of the list
