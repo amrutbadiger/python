ea02eccdf270c7c1da37c48f00f7ee7346b9a9f2
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-23 12:51:45 +0530

    Create list3.py

---
+++ list3.py
@@ -0,0 +1,11 @@
+# deleting and removing elements from lists.
+items = ["sugar", "tea", "salt", "paste", "soap", "rice", "pulses"]
+print(items)
+# eleminating a element using its index number.
+del items[3]
+print(items)
+#append "paste" to the list and eliminate it using its value.
+items.append("paste")
+print(items)
+items.remove("paste")
+print(items)
