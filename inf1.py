c8e08cc5f87b8098aa806caf8023f3ddfd47c7e9
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-25 00:31:25 +0530

    Create inf1.py

---
+++ inf1.py
@@ -0,0 +1,15 @@
+num_entered = input("Enter a number: ")
+decimal_number = float(num_entered)
+total = decimal_number + 11
+print(total)# converting number to float
+age=input("Enter your age: ")# user's input in string
+age_num=int(age)# converting in integer
+a_year_older=age_num+1 # age as will be after a year in intiger
+older_as_str=str(a_year_older) # integer converted in a string
+print("Next year you'll be "+ older_as_str) #displayiig the users age as next year.
+'''
+Code an input statement. Make up the
+variable name and the prompt. Then display the user's answer.
+'''
+fav_movie=input("Enter your fav movie: ")
+print(fav_movie)
