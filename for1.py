0149017d57ae9cfc6842fbf0c4971c235e622c47
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-24 20:02:54 +0530

    Create for1.py

---
+++ for1.py
@@ -0,0 +1,13 @@
+nums = [1, 2, 3]
+for a_num in nums:
+    print(a_num)
+my_street="Main"
+streets =("Elm", "Walnut", "Center", "Main", "Front")
+for a_street in streets:
+    if a_street==my_street:
+        print("yes")
+pet_animal="dog"
+animals =("cow", "donkey", "horse", "dog", "tiger", "lion")
+for a_animal in animals:
+    if a_animal == pet_animal:
+        print("it's ok")
