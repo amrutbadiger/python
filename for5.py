70f06814e1a0742a3323bf0dfe2edd5983c0295f
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-24 23:32:03 +0530

    Create for5.py

---
+++ for5.py
@@ -0,0 +1,18 @@
+outer_loop_count=0
+inner_loop_count=0
+first_name=["Ram", "Krsna", "Govind", "Raju", "Pappu"]
+last_name=["Singh", "Verma", "Mishra", "Swami", "Guru"]
+full_names=[]
+for a_first_name in first_name:
+   for a_last_name in last_name:
+        full_names.append(a_first_name+" " + a_last_name)
+        print(full_names)
+
+ 
+     
+        
+        
+
+
+
+
