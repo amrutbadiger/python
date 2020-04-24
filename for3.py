208c529f33bd0a5b7f276ce5a09ea1c18e31d39d
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-24 22:45:02 +0530

    Create for3.py

---
+++ for3.py
@@ -0,0 +1,10 @@
+'''
+Code an outer loop that loops through the first list
+and an innner loop that loops through the second list.
+The inner loop displays each inner list value.
+'''
+x =[1, 2, 3, 4, 5, 6]
+y =[7, 8, 9, 10, 11]
+for a in x:
+    for b in y:
+        print(b)
