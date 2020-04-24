13e7be928b4e4d121906bc6d4af3bd18139c3748
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-24 21:54:25 +0530

    Create for2,py.py

---
+++ for2,py.py
@@ -0,0 +1,6 @@
+my_city="Belgaum"
+clean_cities=["Mumbai", "Delhi",  "Belgaum", "Chennai",  "Chandigarh"]
+for a_clean_city in  clean_cities:
+    if a_clean_city == my_city:
+        print("It's one of the cleanest cities")
+        break
