fb6e63a55b6586e22bf6e0b9cd0bcb1e79da3e01
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-25 17:32:02 +0530

    Create dict3.py

---
+++ dict3.py
@@ -0,0 +1,27 @@
+'''
+Code a dict breaking into seperate lines.Don't forger to
+indent properly. Add a optional comma to the last pair.
+'''
+dict={
+    1: "one",
+    2: "two",
+    3: "three",
+}
+print(dict)
+'''
+Code a 3 item dict breaking it into 5 seperatelines.
+then display one of the value.
+'''
+nums={
+    "ten": 10,
+    "five": 5,
+    "nine": 9,
+}
+print(nums["nine"])
+# code a dict containing int and floats and display the sum of all values.
+sums={
+    1: 2.5,
+    2: 6.5,
+    3: 9.5,
+}
+print(sums[1]+sums[2]+sums[3])
