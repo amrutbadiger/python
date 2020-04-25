5f321e55bd2cfe9e59a000babe9673f623493105
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-25 13:55:21 +0530

    Create case2.py

---
+++ case2.py
@@ -0,0 +1,25 @@
+# to convert the elements of a list to upper lower cases .
+name =["Bob", "rAm", "SId", "raJ", "JACK"]
+print(name)
+'''
+ to convert all the elements of a list to lowercase
+ ,uppercase or capitalize the first letter
+ using a for loop and dispaying all the elements.
+ '''
+name =["Bob", "rAm", "SId", "raJ", "JACK"]
+for a_name in name:
+    a_name=a_name.lower()
+    print(a_name)
+for a_name in name:
+    a_name=a_name.upper()
+    print(a_name)
+for a_name in name:
+    a_name=a_name.title()
+    print(a_name)
+# using single line coding to convert the strings.
+name ="Bob, rAm, SId, rAj, JACK"
+print(name.title())
+print(name.lower())
+print(name.upper())
+    
+    
