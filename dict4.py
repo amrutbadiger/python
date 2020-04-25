d4e62340958543816abd06044d355180a85fdfb4
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-25 21:16:40 +0530

    Create dict4.py

---
+++ dict4.py
@@ -0,0 +1,8 @@
+# Adding items to a dictionary.
+dict={1: "goa", 2: "belgaum", 3: "hubli",}
+dict["city"]="delhi"
+dict["metro"]="chennai"
+print(dict)
+# add an item to a dictionary , targeting the item display its value.
+dict["gold"]=1282
+print(dict["gold"])
