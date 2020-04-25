6d2393adaac3e9714e573c463dd168f82fe62bc1
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-25 15:46:30 +0530

    Create dict1.py

---
+++ dict1.py
@@ -0,0 +1,11 @@
+# A program to code a dictionary.
+country_capitals = {"India": "NewDelhi", "Pakistan": "Islamabad", "America": "Washington", "Canada": "Montreal"}
+print(country_capitals)
+# to code a dictionary with strings as keys and numbers as value.
+numerals= {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+print(numerals)
+# to code a dictionary with numbers as keys and strings as value.
+metros = {1: "Mumbai", 2: "Chennai", 3: "Delhi", 4: "Kolkatta"}
+print(metros)
+mixed_dict= {1: "Ram", "Krsna": 2, 3: "govind", "Arjun": 4}
+print(mixed_dict)
