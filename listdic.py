9e5878d7eb41c9c58ffbd4e0b0cb52ce97a39e37
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-28 00:45:12 +0530

    Create listdic.py

---
+++ listdic.py
@@ -0,0 +1,19 @@
+# creating a  list of dictionaries.Display it.
+customers =[
+    {
+        "cust_id": 0,
+        "first_name": "Jay",
+        "last_name": "Dixit",
+    },
+    {
+        "cust_id": 1,
+        "first_name": "Raj",
+        "last_name": "Dev",
+    },
+    {
+        "cust_id": 2,
+        "first_name": "Ram",
+        "last_name": "Sinh",
+    },
+]
+print(customers)
