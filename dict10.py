7cc12a7a844f299c1e866d7a773018c573ed88b9
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-29 01:30:57 +0530

    Create dict10.py

---
+++ dict10.py
@@ -0,0 +1,28 @@
+students={
+    0:{
+        "first name": "Ajay",
+        "last name": "Singh",
+        "Age": 25,
+    },
+    1:{
+        "first name": "Raj",
+        "last name": "Dev",
+        "Age": 24,
+    },
+    2:{
+        "first name": "Sam",
+        "last name": "Paul",
+        "Age": 26,
+    },
+    3:{
+        "first name": "Bob",
+        "last name": "Souz",
+        "Age": 23,
+    },
+}
+print(len(students)) # to display how many pairs are there in the dict.
+print(len(students[3])) # to display the no of items in a inner dict.
+print(students[2]) # to display the elements of the inner dict.
+print(students[1]["first name"])# to display the value of a specific key in the inner dict.
+
+
