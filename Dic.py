394afe322d8ac1994efa79e19c367b1211299d29
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-29 00:31:10 +0530

    Create Dic.py

---
+++ Dic.py
@@ -0,0 +1,19 @@
+# to code a dict with an inner dict. Display it.
+dict={
+    1: {
+        "a": 12,
+        "b": 24,
+        "c": 36,
+    },
+}
+print(dict)
+# to code a dict containing a list and a inner dict and display it.
+tools={
+    "items":["wrenches", "saw", "nails", "screw"],
+    "colors":{
+        1: "red",
+        2: "blue",
+        3: "green",
+    },
+}
+print(tools)
