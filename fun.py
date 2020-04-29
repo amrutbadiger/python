1c44289eaace44f9d21d40d215fc276cc6d49d83
amrutbadiger <63974729+amrutbadiger@users.noreply.github.com>
2020-04-29 12:51:47 +0530

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,23 @@
+students={
+    "first name": "Ajay",
+    "last name": "Singh",
+    "profession": "composer",
+}
+def fact():
+    for fact in students.values():
+        print(fact)
+fact()
+# Code a function that tests whether a equals b and, if so, displays ("ok"). 
+a=2
+b=2
+def check():
+    if a==b:
+        print("ok")
+check()
+# Code a function that displays the concatenation of two strings.
+def greet():
+    print("Hello, "+"World")
+greet()
+
+
+
